d8b4b7125b380c0a4547b913598c3b37dfb39366
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-04-17 00:15:53 +0300

    Update Program.cs

--- Life/Program.cs
+++ Life/Program.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace cli_life
 {
@@ -29,23 +31,45 @@ namespace cli_life
     {
         public readonly Cell[,] Cells;
         public readonly int CellSize;
-
-        public int Columns { get { return Cells.GetLength(0); } }
-        public int Rows { get { return Cells.GetLength(1); } }
-        public int Width { get { return Columns * CellSize; } }
-        public int Height { get { return Rows * CellSize; } }
-
-        public Board(int width, int height, int cellSize, double liveDensity = .1)
+        public int Height
         {
-            CellSize = cellSize;
+            get
+            {
+                return Rows * CellSize;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return Columns * CellSize;
+            }
+        }
+        public int Columns
+        {
+            get
+            {
+                return Cells.GetLength(0);
+            }
+        }
+        public int Rows
+        {
+            get
+            {
+                return Cells.GetLength(1);
+            }
+        }
+        public Board(Set c)
+        {
+            CellSize = c.CellSize;
 
-            Cells = new Cell[width / cellSize, height / cellSize];
-            for (int x = 0; x < Columns; x++)
-                for (int y = 0; y < Rows; y++)
-                    Cells[x, y] = new Cell();
+            Cells = new Cell[c.Width / c.CellSize, c.Height / c.CellSize];
+            for (int i = 0; i < Columns; i++)
+                for (int j = 0; j < Rows; j++)
+                    Cells[i, j] = new Cell();
 
             ConnectNeighbors();
-            Randomize(liveDensity);
+            Randomize(c.LiveDensity);
         }
 
         readonly Random rand = new Random();
@@ -86,25 +110,115 @@ namespace cli_life
             }
         }
     }
-    class Program
+    public class Set
     {
-        static Board board;
-        static private void Reset()
+        struct Data
         {
-            board = new Board(
-                width: 50,
-                height: 20,
-                cellSize: 1,
-                liveDensity: 0.5);
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public int CellSize { get; set; }
+            public double LiveDensity { get; set; }
         }
-        static void Render()
+        Data data;
+        public int Height
         {
+            get
+            {
+                return data.Height;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return data.Width;
+            }
+        }
+
+        public int CellSize
+        {
+            get
+            {
+                return data.CellSize;
+            }
+        }
+        public double LiveDensity
+        {
+            get
+            {
+                return data.LiveDensity;
+            }
+        }
+        public void Conf(String s)
+        {
+            data = JsonConvert.DeserializeObject<Data>(File.ReadAllText(s));
+        }
+        public void LoadState(Board board, String s)
+        {
+            String b = File.ReadAllText(s);
+            int i = 0;
+            int j = -1;
+            foreach (char c in b)
+            {
+                j += 1;
+                if ((j == 50) && (i == 19))
+                {
+                    break;
+                }
+                if (c == '*')
+                {
+                    board.Cells[j, i].IsAlive = true;
+                }
+                if (c == ' ')
+                {
+                    board.Cells[j, i].IsAlive = false;
+                }
+                if (c == '\n')
+                {
+                    i += 1;
+                    j = -1;
+                }
+
+            }
+        }
+        public void SaveState(Board board, String s)
+        {
+            String b = "";
             for (int row = 0; row < board.Rows; row++)
             {
-                for (int col = 0; col < board.Columns; col++)   
+                for (int col = 0; col < board.Columns; col++)
                 {
                     var cell = board.Cells[col, row];
                     if (cell.IsAlive)
+                    {
+                        b += "*";
+                    }
+                    else
+                    {
+                        b += " ";
+                    }
+                }
+                b += "\n";
+            }
+            File.WriteAllText(s, b);
+        }
+    }
+
+    class Program
+    {
+        static Board board;
+        static private void Reboot(Set c)
+        {
+            board = new Board(c);
+        }
+        static void Show()
+        {
+            for (int i = 0; i < board.Rows; i++)
+            {
+                for (int j = 0; j < board.Columns; j++)
+                {
+                    var c = board.Cells[j, i];
+                    if (c.IsAlive)
                     {
                         Console.Write('*');
                     }
@@ -116,16 +230,101 @@ namespace cli_life
                 Console.Write('\n');
             }
         }
+
         static void Main(string[] args)
         {
-            Reset();
-            while(true)
+            int num = 0;
+            Set c = new Set();
+            c.Conf("D:\\ConsoleApp1\\form.json");
+            Reboot(c);
+            c.LoadState(board, "D:\\ConsoleApp1\\data.txt");
+            while ((true) && (num < 10))
             {
+                num++;
                 Console.Clear();
-                Render();
+                Show();
+                c.SaveState(board, "D:\\ConsoleApp1\\data.txt");
                 board.Advance();
                 Thread.Sleep(1000);
             }
+            do
+            {
+                Console.WriteLine("Choose your number: 1 - Мигалки, 2 - Ящик, 3 - Улей, 4 - Каравай, 5 - Корабль");
+                int choice = 0;
+                while (choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5)
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                switch (choice)
+                {
+                    case 1:
+                        num = 0;
+                        c.LoadState(board, "D:\\ConsoleApp1\\1.txt");
+                        while ((true) && (num < 5))
+                        {
+                            num++;
+                            Console.Clear();
+                            Show();
+                            c.SaveState(board, "D:\\ConsoleApp1\\1.txt");
+                            board.Advance();
+                            Thread.Sleep(1000);
+                        }
+                        break;
+                    case 2:
+                        num = 0;
+                        c.LoadState(board, "D:\\ConsoleApp1\\2.txt");
+                        while ((true) && (num < 5))
+                        {
+                            num++;
+                            Console.Clear();
+                            Show();
+                            c.SaveState(board, "D:\\ConsoleApp1\\2.txt");
+                            board.Advance();
+                            Thread.Sleep(1000);
+                        }
+                        break;
+                    case 3:
+                        num = 0;
+                        c.LoadState(board, "D:\\ConsoleApp1\\3.txt");
+                        while ((true) && (num < 5))
+                        {
+                            num++;
+                            Console.Clear();
+                            Show();
+                            c.SaveState(board, "D:\\ConsoleApp1\\3.txt");
+                            board.Advance();
+                            Thread.Sleep(1000);
+                        }
+                        break;
+                    case 4:
+                        num = 0;
+                        c.LoadState(board, "D:\\ConsoleApp1\\4.txt");
+                        while ((true) && (num < 5))
+                        {
+                            num++;
+                            Console.Clear();
+                            Show();
+                            c.SaveState(board, "D:\\ConsoleApp1\\4.txt");
+                            board.Advance();
+                            Thread.Sleep(1000);
+                        }
+                        break;
+                    case 5:
+                        num = 0;
+                        c.LoadState(board, "D:\\ConsoleApp1\\5.txt");
+                        while ((true) && (num < 5))
+                        {
+                            num++;
+                            Console.Clear();
+                            Show();
+                            c.SaveState(board, "D:\\ConsoleApp1\\5.txt");
+                            board.Advance();
+                            Thread.Sleep(1000);
+                        }
+                        break;
+                }
+            } while (true);
+
         }
     }
 }
