40f04f6114d1d48aea4baff3da6772e3b8813456
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-04-22 17:55:08 +0300

    add settings

---
+++ Life/Settings.cs
@@ -0,0 +1,8 @@
+﻿class Settings
+{
+    public static int width = 10;
+    public static int height = 10;
+    public static int cellSize = 1;
+    public static double liveDensity = 0.5;
+    public static int option = 2;
+}
