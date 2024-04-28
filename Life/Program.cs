2e0de4acc6d0331701d9ead6275c5afd027094f4
Виктор Финаев <finaev-2001@mail.ru>
2024-04-28 18:35:20 +0300

    Доработка консольного приложения. + Добавлена поддержка JSON-файла с настройками КЛА. + Добавлена возможность сохранения состояния системы в текстовый файл. + Добавлена возможность загрузки состояния системы из текстового файла.

--- Life/Program.cs
+++ Life/Program.cs
@@ -4,128 +4,473 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace cli_life
 {
+    /// <summary>
+    /// Настройки доски.
+    /// </summary>
+    public class BoardSettings
+    {
+        /// <summary>
+        /// Ширина доски.
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Высота доски.
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Размер клетки.
+        /// </summary>
+        public int CellSize { get; set; }
+
+        /// <summary>
+        /// Плотность жизни.
+        /// </summary>
+        public double LiveDensity { get; set; }
+    }
+
+    /// <summary>
+    /// Клетка.
+    /// </summary>
     public class Cell
     {
+        /// <summary>
+        /// Жива ли клетка.
+        /// </summary>
         public bool IsAlive;
-        public readonly List<Cell> neighbors = new List<Cell>();
+
+        /// <summary>
+        /// Соседи клетки.
+        /// </summary>
+        public readonly List<Cell> neighbors = new();
+
+        /// <summary>
+        /// Будет ли клетка жива в следующем поколении.
+        /// </summary>
         private bool IsAliveNext;
+
+        /// <summary>
+        /// Определяет следующее состояние клетки.
+        /// </summary>
         public void DetermineNextLiveState()
         {
             int liveNeighbors = neighbors.Where(x => x.IsAlive).Count();
+
             if (IsAlive)
                 IsAliveNext = liveNeighbors == 2 || liveNeighbors == 3;
             else
                 IsAliveNext = liveNeighbors == 3;
         }
+
+        /// <summary>
+        /// Определяет состояние клетки на следующем шаге.
+        /// </summary>
         public void Advance()
         {
             IsAlive = IsAliveNext;
         }
     }
+
+    /// <summary>
+    /// Доска симуляции жизни.
+    /// </summary>
     public class Board
     {
-        public readonly Cell[,] Cells;
-        public readonly int CellSize;
+        /// <summary>
+        /// Ширина доски.
+        /// </summary>
+        private int _width;
 
-        public int Columns { get { return Cells.GetLength(0); } }
-        public int Rows { get { return Cells.GetLength(1); } }
-        public int Width { get { return Columns * CellSize; } }
-        public int Height { get { return Rows * CellSize; } }
+        /// <summary>
+        /// Высота доски.
+        /// </summary>
+        private int _heigth;
 
+        /// <summary>
+        /// Массив клеток.
+        /// </summary>
+        public Cell[,] Cells;
+
+        /// <summary>
+        /// Размер клетки.
+        /// </summary>
+        public int CellSize;
+
+        /// <summary>
+        /// Число колонок массива клеток.
+        /// </summary>
+        public int Columns { get { return Cells.GetLength(1); } }
+
+        /// <summary>
+        /// Число строк массива клеток.
+        /// </summary>
+        public int Rows { get { return Cells.GetLength(0); } }
+
+        /// <summary>
+        /// Ширина доски.
+        /// </summary>
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            private set
+            {
+                _width = value;
+            }
+        }
+
+        /// <summary>
+        /// Высота доски.
+        /// </summary>
+        public int Height
+        {
+            get
+            {
+                return _heigth;
+            }
+            private set
+            {
+                _heigth = value;
+            }
+        }
+
+        /// <summary>
+        /// Создает экземпляр доски.
+        /// </summary>
+        /// <param name="width">Ширина доски.</param>
+        /// <param name="height">Высота доски.</param>
+        /// <param name="cellSize">Размер клетки.</param>
+        /// <param name="liveDensity">Плотность жизни.</param>
         public Board(int width, int height, int cellSize, double liveDensity = .1)
         {
             CellSize = cellSize;
 
-            Cells = new Cell[width / cellSize, height / cellSize];
-            for (int x = 0; x < Columns; x++)
-                for (int y = 0; y < Rows; y++)
-                    Cells[x, y] = new Cell();
+            Cells = new Cell[height / cellSize, width / cellSize];
+
+            Width = Columns * CellSize;
+            Height = Rows * CellSize;
+
+            for (var row = 0; row < Rows; row++)
+            {
+                for (var col = 0; col < Columns; col++)
+                {
+                    Cells[row, col] = new Cell();
+                }
+            }
 
             ConnectNeighbors();
             Randomize(liveDensity);
         }
 
-        readonly Random rand = new Random();
-        public void Randomize(double liveDensity)
+        /// <summary>
+        /// Создает экземпляр доски с параметрами по умолчанию.
+        /// </summary>
+        public Board() : this(50, 20, 1, 0.5)
         {
-            foreach (var cell in Cells)
-                cell.IsAlive = rand.NextDouble() < liveDensity;
         }
 
+        /// <summary>
+        /// Заполняет доску случайным образом.
+        /// </summary>
+        /// <param name="liveDensity">Плотность жизни.</param>
+        public void Randomize(double liveDensity)
+        {
+            var rand = new Random();
+
+            foreach (var cell in Cells)
+            {
+                cell.IsAlive = rand.NextDouble() < liveDensity;
+            }
+        }
+
+        /// <summary>
+        /// Определяет состояние доски на следующем шаге симуляции.
+        /// </summary>
         public void Advance()
         {
             foreach (var cell in Cells)
+            {
                 cell.DetermineNextLiveState();
+            }
+
             foreach (var cell in Cells)
+            {
                 cell.Advance();
+            }
         }
+
+        /// <summary>
+        /// Соединяет соседей клеток.
+        /// </summary>
         private void ConnectNeighbors()
         {
-            for (int x = 0; x < Columns; x++)
+            for (var row = 0; row < Rows; row++)
             {
-                for (int y = 0; y < Rows; y++)
+                for (var col = 0; col < Columns; col++)
                 {
-                    int xL = (x > 0) ? x - 1 : Columns - 1;
-                    int xR = (x < Columns - 1) ? x + 1 : 0;
+                    var rowBottom = (row > 0)
+                        ? row - 1
+                        : Rows - 1;
+                    var rowTop = (row < Rows - 1)
+                        ? row + 1
+                        : 0;
 
-                    int yT = (y > 0) ? y - 1 : Rows - 1;
-                    int yB = (y < Rows - 1) ? y + 1 : 0;
+                    var colLeft = (col > 0)
+                        ? col - 1
+                        : Columns - 1;
+                    var colRight = (col < Columns - 1)
+                        ? col + 1
+                        : 0;
 
-                    Cells[x, y].neighbors.Add(Cells[xL, yT]);
-                    Cells[x, y].neighbors.Add(Cells[x, yT]);
-                    Cells[x, y].neighbors.Add(Cells[xR, yT]);
-                    Cells[x, y].neighbors.Add(Cells[xL, y]);
-                    Cells[x, y].neighbors.Add(Cells[xR, y]);
-                    Cells[x, y].neighbors.Add(Cells[xL, yB]);
-                    Cells[x, y].neighbors.Add(Cells[x, yB]);
-                    Cells[x, y].neighbors.Add(Cells[xR, yB]);
+                    Cells[row, col].neighbors.Add(Cells[rowTop, colLeft]);
+                    Cells[row, col].neighbors.Add(Cells[rowTop, col]);
+                    Cells[row, col].neighbors.Add(Cells[rowTop, colRight]);
+                    Cells[row, col].neighbors.Add(Cells[row, colLeft]);
+                    Cells[row, col].neighbors.Add(Cells[row, colRight]);
+                    Cells[row, col].neighbors.Add(Cells[rowBottom, colLeft]);
+                    Cells[row, col].neighbors.Add(Cells[rowBottom, col]);
+                    Cells[row, col].neighbors.Add(Cells[rowBottom, colRight]);
                 }
             }
         }
+
+        /// <summary>
+        /// Записывает состояние доски в указанный файл.
+        /// </summary>
+        /// <param name="filePath">Путь к файлу.</param>
+        public void SaveBoardState(string filePath)
+        {
+            var boardStringRepr = string.Empty;
+
+            for (var row = 0; row < Height; row++)
+            {
+                for (var col = 0; col < Width; col++)
+                {
+                    boardStringRepr +=
+                        (Cells[row, col].IsAlive)
+                            ? '*'
+                            : ' ';
+                }
+                boardStringRepr += '\n';
+            }
+            boardStringRepr += $"cellSize={CellSize}";
+
+            File.WriteAllText(filePath, boardStringRepr);
+        }
+
+        /// <summary>
+        /// Загружает состояние доски из файла.
+        /// </summary>
+        /// <param name="filePath">Путь к файлу.</param>
+        public void LoadBoardState(string filePath)
+        {
+            var boardStringArrayRepr = File.ReadAllLines(filePath);
+
+            int.TryParse(
+                boardStringArrayRepr[boardStringArrayRepr.Length - 1]
+                    .Substring(
+                        boardStringArrayRepr[boardStringArrayRepr.Length - 1]
+                        .IndexOf('=') + 1),
+                out int cellSize);
+
+            CellSize = cellSize > 1
+                ? cellSize
+                : 1;
+
+            Height = boardStringArrayRepr.Length - 1;
+            Width = boardStringArrayRepr[0].Length;
+
+            Cells = new Cell[Height / CellSize, Width / CellSize];
+
+            for (var row = 0; row < Height; row += CellSize)
+            {
+                for (var col = 0; col < Width; col += CellSize)
+                {
+                    Cells[row / CellSize, col / CellSize] = new Cell
+                    {
+                        IsAlive = (boardStringArrayRepr[row][col] == '*')
+                    };
+                }
+            }
+
+            ConnectNeighbors();
+        }
     }
+
+    /// <summary>
+    /// Консольное приложение.
+    /// </summary>
     class Program
     {
-        static Board board;
+        /// <summary>
+        /// Путь к файлу с настройками.
+        /// </summary>
+        const string JsonPath = "boardSettings.json";
+
+        /// <summary>
+        /// Файл с состоянием доски.
+        /// </summary>
+        const string BoardStateFilePath = "gameOfLife_sf1.txt";
+
+        /// <summary>
+        /// Число итераций симуляции.
+        /// </summary>
+        static int IterationsNum = 0;
+
+        /// <summary>
+        /// Выполнять загрузку из файла.
+        /// </summary>
+        static bool LoadFromFile = false;
+
+        /// <summary>
+        /// Сохранять результат в файл.
+        /// </summary>
+        static bool SaveToFile = false;
+
+        /// <summary>
+        /// Доска с симуляцией жизни.
+        /// </summary>
+        static Board SimulationBoard;
+
+        /// <summary>
+        /// Загрузить настройки доски из JSON-файла.
+        /// </summary>
+        /// <returns>Настройки доски.</returns>
+        static private BoardSettings LoadBoardSettings()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            string json = File.ReadAllText(JsonPath);
+            return JsonSerializer.Deserialize<BoardSettings>(json);
+        }
+
+        /// <summary>
+        /// Сбрасывает настройки доски.
+        /// </summary>
         static private void Reset()
         {
-            board = new Board(
-                width: 50,
-                height: 20,
-                cellSize: 1,
-                liveDensity: 0.5);
+            try
+            {
+                var settings = LoadBoardSettings();
+
+                SimulationBoard = new Board(
+                    width: settings.Width,
+                    height: settings.Height,
+                    cellSize: settings.CellSize,
+                    liveDensity: settings.LiveDensity);
+            }
+            catch
+            {
+                SimulationBoard = new Board();
+            }
         }
+
+        /// <summary>
+        /// Отрисовывает доску.
+        /// </summary>
         static void Render()
         {
-            for (int row = 0; row < board.Rows; row++)
+            for (var row = 0; row < SimulationBoard.Rows; row++)
             {
-                for (int col = 0; col < board.Columns; col++)   
+                for (var col = 0; col < SimulationBoard.Columns; col++)
                 {
-                    var cell = board.Cells[col, row];
-                    if (cell.IsAlive)
-                    {
-                        Console.Write('*');
-                    }
-                    else
-                    {
-                        Console.Write(' ');
-                    }
+                    var cell = SimulationBoard.Cells[row, col];
+
+                    Console.Write(cell.IsAlive
+                        ? '*'
+                        : ' ');
                 }
+
                 Console.Write('\n');
             }
         }
-        static void Main(string[] args)
+
+        /// <summary>
+        /// Выполняет предварительную настройку симуляции через взаимодействие с пользователем.
+        /// </summary>
+        static void ConfigureSimulationStart()
         {
-            Reset();
-            while(true)
+            Console.Write("Введите число итераций симуляции: ");
+            int.TryParse(Console.ReadLine(), out IterationsNum);
+
+            Console.Write("Выполнять загрузку из файла? (1 - да, 0 - нет): ");
+            int.TryParse(Console.ReadLine(), out int loadFileResponse);
+            LoadFromFile =
+                loadFileResponse == 0
+                    ? false
+                    : true;
+
+            Console.Write("Выполнять сохранение результата в файл? (1 - да, 0 - нет): ");
+            int.TryParse(Console.ReadLine(), out int saveFileResponse);
+            SaveToFile =
+                saveFileResponse == 0
+                    ? false
+                    : true;
+        }
+
+        /// <summary>
+        /// Запускает симуляцию в соответствии с настроенной конфигурацией.
+        /// </summary>
+        static void RunSimulation()
+        {
+            if (LoadFromFile)
+            {
+                try
+                {
+                    SimulationBoard.LoadBoardState(BoardStateFilePath);
+                    Console.WriteLine("Успешное чтение данных из файла.");
+                }
+                catch
+                {
+                    Reset();
+                    Console.WriteLine("Ошибка при чтении данных из файла.");
+                }
+
+                Thread.Sleep(2000);
+            }
+
+            for (var index = 0; index < IterationsNum; index++)
             {
                 Console.Clear();
                 Render();
-                board.Advance();
-                Thread.Sleep(1000);
+                SimulationBoard.Advance();
+                Thread.Sleep(100);
             }
+
+            if (SaveToFile)
+            {
+                try
+                {
+                    SimulationBoard.SaveBoardState(BoardStateFilePath);
+                    Console.WriteLine("Успешное сохранение результата в файл.");
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка при сохранении результата в файл.");
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Точка входа программы.
+        /// </summary>
+        /// <param name="args">Передаваемые аргументы.</param>
+        static void Main(string[] args)
+        {
+            ConfigureSimulationStart();
+            Reset();
+
+            RunSimulation();
         }
     }
 }
