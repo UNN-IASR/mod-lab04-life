4a3c2fef7d7ead3e876ef10f95cdafba98769ebc
rshahkov <148948376+rshahkov@users.noreply.github.com>
2024-05-11 19:08:49 +0300

    Complete with out test Program.cs

--- Life/Program.cs
+++ Life/Program.cs
@@ -1,112 +1,224 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.IO;
+using System.Text.Json;
 
-namespace cli_life
+namespace LifeSimulation
 {
-    public class Cell
+    public class LifeData
     {
-        public bool IsAlive;
-        public readonly List<Cell> neighbors = new List<Cell>();
-        private bool IsAliveNext;
-        public void DetermineNextLiveState()
+        public int gridWidth { get; set; }
+        public int gridHeight { get; set; }
+        public int cellDimension { get; set; }
+        public double livingCellsRatio { get; set; }
+    }
+
+    public class LifeCell
+    {
+        public bool isActive;
+        public readonly List<LifeCell> surroundingCells = new List<LifeCell>();
+        private bool willBeActive;
+
+        public void CalculateNextState()
         {
-            int liveNeighbors = neighbors.Where(x => x.IsAlive).Count();
-            if (IsAlive)
-                IsAliveNext = liveNeighbors == 2 || liveNeighbors == 3;
+            int activeNeighbors = surroundingCells.Count(x => x.isActive);
+            if (isActive)
+                willBeActive = activeNeighbors == 2 || activeNeighbors == 3;
             else
-                IsAliveNext = liveNeighbors == 3;
+                willBeActive = activeNeighbors == 3;
         }
-        public void Advance()
+
+        public void UpdateState()
         {
-            IsAlive = IsAliveNext;
+            isActive = willBeActive;
         }
     }
-    public class Board
+
+    public class LifeGrid
     {
-        public readonly Cell[,] Cells;
-        public readonly int CellSize;
+        public readonly LifeCell[,] gridCells;
+        public readonly int cellDimension;
 
-        public int Columns { get { return Cells.GetLength(0); } }
-        public int Rows { get { return Cells.GetLength(1); } }
-        public int Width { get { return Columns * CellSize; } }
-        public int Height { get { return Rows * CellSize; } }
+        public int GridColumns { get { return gridCells.GetLength(0); } }
+        public int GridRows { get { return gridCells.GetLength(1); } }
+        public int GridWidth { get { return GridColumns * cellDimension; } }
+        public int GridHeight { get { return GridRows * cellDimension; } }
 
-        public Board(int width, int height, int cellSize, double liveDensity = .1)
+        public LifeGrid(int gridWidth, int gridHeight, int cellDimension, double livingCellsRatio)
         {
-            CellSize = cellSize;
+            cellDimension = cellDimension;
 
-            Cells = new Cell[width / cellSize, height / cellSize];
-            for (int x = 0; x < Columns; x++)
-                for (int y = 0; y < Rows; y++)
-                    Cells[x, y] = new Cell();
+            gridCells = new LifeCell[gridWidth / cellDimension, gridHeight / cellDimension];
+            for (int x = 0; x < GridColumns; x++)
+                for (int y = 0; y < GridRows; y++)
+                    gridCells[x, y] = new LifeCell();
 
-            ConnectNeighbors();
-            Randomize(liveDensity);
+            ConnectSurroundingCells();
         }
 
         readonly Random rand = new Random();
-        public void Randomize(double liveDensity)
-        {
-            foreach (var cell in Cells)
-                cell.IsAlive = rand.NextDouble() < liveDensity;
-        }
 
-        public void Advance()
+        public void LoadCellsFromFile(string filePath)
         {
-            foreach (var cell in Cells)
-                cell.DetermineNextLiveState();
-            foreach (var cell in Cells)
-                cell.Advance();
-        }
-        private void ConnectNeighbors()
-        {
-            for (int x = 0; x < Columns; x++)
+            string[] lines = File.ReadAllLines(filePath);
+            char[][] gridArray = new char[GridRows][];
+            for (int i = 0; i < lines.Length; i++)
             {
-                for (int y = 0; y < Rows; y++)
+                gridArray[i] = new char[GridColumns];
+                for (int j = 0; j < GridRows; j++)
                 {
-                    int xL = (x > 0) ? x - 1 : Columns - 1;
-                    int xR = (x < Columns - 1) ? x + 1 : 0;
-
-                    int yT = (y > 0) ? y - 1 : Rows - 1;
-                    int yB = (y < Rows - 1) ? y + 1 : 0;
-
-                    Cells[x, y].neighbors.Add(Cells[xL, yT]);
-                    Cells[x, y].neighbors.Add(Cells[x, yT]);
-                    Cells[x, y].neighbors.Add(Cells[xR, yT]);
-                    Cells[x, y].neighbors.Add(Cells[xL, y]);
-                    Cells[x, y].neighbors.Add(Cells[xR, y]);
-                    Cells[x, y].neighbors.Add(Cells[xL, yB]);
-                    Cells[x, y].neighbors.Add(Cells[x, yB]);
-                    Cells[x, y].neighbors.Add(Cells[xR, yB]);
+                    gridArray[i][j] = lines[i][j];
+                }
+            }
+            for (int i = 0; i < GridRows; i++)
+            {
+                for (int j = 0; j < GridColumns; j++)
+                {
+                    if (gridArray[i][j] == '*')
+                    {
+                        gridCells[i, j].isActive = true;
+                    }
                 }
             }
         }
-    }
-    class Program
-    {
-        static Board board;
-        static private void Reset()
+
+        public void RandomizeGrid(string filePath)
         {
-            board = new Board(
-                width: 50,
-                height: 20,
-                cellSize: 1,
-                liveDensity: 0.5);
+            string jsonContent = File.ReadAllText(filePath);
+            LifeData data = JsonSerializer.Deserialize<LifeData>(jsonContent);
+            double livingCellsRatio = data.livingCellsRatio;
+            foreach (var cell in gridCells)
+                cell.isActive = rand.NextDouble() < livingCellsRatio;
         }
-        static void Render()
+
+        public void AdvanceGrid()
         {
-            for (int row = 0; row < board.Rows; row++)
+            foreach (var cell in gridCells)
+                cell.CalculateNextState();
+            foreach (var cell in gridCells)
+                cell.UpdateState();
+        }
+
+        private void ConnectSurroundingCells()
+        {
+            for (int x = 0; x < GridColumns; x++)
             {
-                for (int col = 0; col < board.Columns; col++)   
+                for (int y = 0; y < GridRows; y++)
                 {
-                    var cell = board.Cells[col, row];
-                    if (cell.IsAlive)
+                    int xLeft = (x > 0) ? x - 1 : GridColumns - 1;
+                    int xRight = (x < GridColumns - 1) ? x + 1 : 0;
+
+                    int yTop = (y > 0) ? y - 1 : GridRows - 1;
+                    int yBottom = (y < GridRows - 1) ? y + 1 : 0;
+
+                    gridCells[x, y].surroundingCells.Add(gridCells[xLeft, yTop]);
+                    gridCells[x, y].surroundingCells.Add(gridCells[x, yTop]);
+                    gridCells[x, y].surroundingCells.Add(gridCells[xRight, yTop]);
+                    gridCells[x, y].surroundingCells.Add(gridCells[xLeft, y]);
+                    gridCells[x, y].surroundingCells.Add(gridCells[xRight, y]);
+                    gridCells[x, y].surroundingCells.Add(gridCells[xLeft, yBottom]);
+                    gridCells[x, y].surroundingCells.Add(gridCells[x, yBottom]);
+                    gridCells[x, y].surroundingCells.Add(gridCells[xRight, yBottom]);
+                }
+            }
+        }
+
+        public int CountBlocks()
+        {
+            int count = 0;
+            for (int i = 1; i < GridRows - 2; i++)
+            {
+                for (int j = 1; j < GridColumns - 2; j++)
+                {
+                    if (gridCells[j, i].isActive && gridCells[j, i + 1].isActive && gridCells[j + 1, i].isActive && gridCells[j + 1, i + 1].isActive)
+                    {
+                        if (!gridCells[j - 1, i - 1].isActive && !gridCells[j, i - 1].isActive && !gridCells[j + 1, i - 1].isActive && !gridCells[j + 2, i - 1].isActive
+                        && !gridCells[j - 1, i + 2].isActive && !gridCells[j, i + 2].isActive && !gridCells[j + 1, i + 2].isActive && !gridCells[j + 2, i + 2].isActive
+                        && !gridCells[j - 1, i].isActive && !gridCells[j + 2, i].isActive && !gridCells[j - 1, i + 2].isActive && !gridCells[j + 2, i + 2].isActive)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+
+        public int CountBoxes()
+        {
+            int count = 0;
+            for (int i = 0; i < GridRows - 2; i++)
+            {
+                for (int j = 1; j < GridColumns - 1; j++)
+                {
+                    if (gridCells[j, i].isActive && gridCells[j - 1, i + 1].isActive && gridCells[j + 1, i + 1].isActive && gridCells[j, i + 2].isActive
+                    && !gridCells[j, i + 1].isActive && !gridCells[j - 1, i].isActive && !gridCells[j + 1, i].isActive && !gridCells[j - 1, i + 2].isActive && !gridCells[j + 1, i + 2].isActive)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public int CountHives()
+        {
+            int count = 0;
+            for (int i = 0; i < GridRows - 3; i++)
+            {
+                for (int j = 1; j < GridColumns - 1; j++)
+                {
+                    if (gridCells[j, i].isActive && gridCells[j - 1, i + 1].isActive && gridCells[j - 1, i + 2].isActive
+                    && gridCells[j, i + 3].isActive && gridCells[j + 1, i + 1].isActive && gridCells[j + 1, i + 2].isActive
+                    && !gridCells[j, i + 1].isActive && !gridCells[j, i + 2].isActive && !gridCells[j - 1, i].isActive
+                    && !gridCells[j + 1, i].isActive && !gridCells[j - 1, i + 3].isActive && !gridCells[j + 1, i + 3].isActive)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public int CountSymmetryFigures()
+        {
+            return CountBlocks() + CountBoxes() + CountHives();
+        }
+    }
+
+    public class LifeSimulation
+    {
+        static LifeGrid grid;
+
+        public int Initialize(string filePath, string settingsPath)
+        {
+            string jsonContent = File.ReadAllText(settingsPath);
+            LifeData data = JsonSerializer.Deserialize<LifeData>(jsonContent);
+            int gridWidth = data.gridWidth;
+            int gridHeight = data.gridHeight;
+            int cellDimension = data.cellDimension;
+            double livingCellsRatio = data.livingCellsRatio;
+            grid = new LifeGrid(gridWidth, gridHeight, cellDimension, livingCellsRatio);
+            grid.LoadCellsFromFile(filePath);
+            return grid.GridWidth * grid.GridHeight;
+        }
+
+        public int Display()
+        {
+            int count = 0;
+            for (int i = 0; i < grid.GridRows; i++)
+            {
+                for (int j = 0; j < grid.GridColumns; j++)
+                {
+                    var cell = grid.gridCells[j, i];
+                    if (cell.isActive)
                     {
                         Console.Write('*');
+                        count++;
                     }
                     else
                     {
@@ -115,17 +227,94 @@ namespace cli_life
                 }
                 Console.Write('\n');
             }
+            return count;
         }
-        static void Main(string[] args)
+
+
+        public void SaveToFile()
         {
-            Reset();
-            while(true)
+            // Создаем список для хранения строк сетки
+            List<List<char>> gridLines = new List<List<char>>();
+
+            // Заполняем список строк сетки, соответствующими текущим размерам сетки
+            for (int i = 0; i < grid.GridRows; i++)
             {
-                Console.Clear();
-                Render();
-                board.Advance();
-                Thread.Sleep(1000);
+                List<char> line = new List<char>();
+                for (int j = 0; j < grid.GridColumns; j++)
+                {
+                    var cell = grid.gridCells[j, i];
+                    if (cell.isActive)
+                    {
+                        line.Add('*');
+                    }
+                    else
+                    {
+                        line.Add(' ');
+                    }
+                }
+                gridLines.Add(line);
             }
+
+            // Сохраняем сетку в файл
+            File.Create("User_sett/result4.txt").Close();
+            using (StreamWriter writer = new StreamWriter("User_sett/result4.txt", true))
+            {
+                foreach (var line in gridLines)
+                {
+                    writer.WriteLine(string.Join("", line));
+                }
+            }
+        }
+
+        public (int allCells, int aliveCells, int Iters) Run(string filePath, string settingsPath)
+        {
+            int[] list = { -1, -1, -1, -1, -1 };
+            int iters = 0;
+            int alive_cells = 0;
+            int all_cells = 0;
+
+            all_cells = Initialize(filePath, settingsPath);
+
+            while (true)
+            {
+                iters++;
+                Console.Clear();
+                alive_cells = Display();
+                list[iters % 5] = alive_cells;
+                if ((list[0] == list[1]) && (list[0] == list[2]) && (list[0] == list[3]) && (list[0] == list[4]))
+                {
+                    break;
+                }
+                grid.AdvanceGrid();
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("\n\tКоличество блоков: " + grid.CountBlocks());
+            Console.WriteLine("\tКоличество ящиков: " + grid.CountBoxes());
+            Console.WriteLine("\tКоличество ульев: " + grid.CountHives());
+
+            (int, int, int) cells = (all_cells, alive_cells, iters - 2);
+            return cells;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {// Установка текущей рабочей директории на директорию, где находится файл
+            Directory.SetCurrentDirectory(@"C:\Users\rshah\Desktop\4 курс\2 семестр\Shtanyk\Lab_4");
+            
+            LifeSimulation simulation = new LifeSimulation();
+            var cells = simulation.Run("User_sett/example4.txt", @"User_sett\user_settings.json");
+
+            Console.Write("\n\tКоличество живых клеток: " + cells.aliveCells);
+            Console.Write("\n\tКоличество мертвых клеток: " + (cells.allCells - cells.aliveCells));
+            Console.Write("\n\tПлотность живых клеток: " + ((double)cells.aliveCells / cells.allCells));
+            Console.Write("\n\tПлотность живых клеток: " + ((double)(cells.allCells - cells.aliveCells) / cells.allCells));
+
+            simulation.SaveToFile();
+
+            Console.Write("\n\n\tСтабильность на " + (cells.Iters) + " итерации.\n\n");
         }
     }
 }
