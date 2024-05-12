54a5cb1d37bf55f2c838710ac15bf2926c830589
Egor Logutov <sv9to0slav@gmail.com>
2024-05-12 20:47:56 +0300

    main

--- cli_life_tests/obj/Debug/net8.0/cli_life_tests.AssemblyInfo.cs
+++ cli_life_tests/obj/Debug/net8.0/cli_life_tests.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Life")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("cli_life_tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b61e9bfbe057be37b6976eb788390188b63339f")]
-[assembly: System.Reflection.AssemblyProductAttribute("Life")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Life")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfad8d69b8a604858e85a955be3e69771aa4be34")]
+[assembly: System.Reflection.AssemblyProductAttribute("cli_life_tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("cli_life_tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
