123eccb82551fa3ebb392d0061d47671c027fdfe
MolkovAlexandr <163853240+MolkovAlexandr@users.noreply.github.com>
2024-05-12 20:20:34 +0300

    Add files via upload

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace mod_lab04_Molkov.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var board = new Board(
+                width: 50,
+                height: 20,
+                cellSize: 1,
+                liveDensity: 0.1 * (i + 1));
+        }
+    }
+}
