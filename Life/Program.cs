93dd80ce7a12ab907e990a0d601a8001d535b576
MolkovAlexandr <163853240+MolkovAlexandr@users.noreply.github.com>
2024-05-12 19:29:36 +0300

    Update Program.cs

--- Life/Program.cs
+++ Life/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Text.Json;
+using ScottPlot;
+using Newtonsoft.Json;
 
 namespace cli_life
 {
@@ -86,7 +89,7 @@ namespace cli_life
             }
         }
     }
-    class Program
+    public class Program
     {
         static Board board;
         static private void Reset()
@@ -97,6 +100,42 @@ namespace cli_life
                 cellSize: 1,
                 liveDensity: 0.5);
         }
+
+        //Количество живых элементов на каждой итерации
+        public static int ElementsAmount(Board b)
+        {
+            int cnt = 0;
+            for (int x = 0; x < b.Columns; x++)
+            {
+                for (int y = 0; y < b.Rows; y++)
+                {
+                    if (b.Cells[x, y].IsAlive) cnt++;
+                }
+            }
+            return cnt;
+        }
+
+        //Сохранение результатов последнего эксперимента
+        public static void SaveResult(String file)
+        {
+            bool[][] result = new bool[board.Columns][];
+            for (int x = 0; x < board.Columns; x++) 
+            {
+                result[x] = new bool[board.Rows];
+                for (int y = 0; y < board.Rows; y++) 
+                {
+                    result[x][y] = board.Cells[x, y].IsAlive;
+                }
+            }
+            File.WriteAllText(file, System.Text.Json.JsonSerializer.Serialize(result));
+        }
+
+        //Сохранение настроек последнего эксперимента
+        public static void SaveSettings(String file, Board b)
+        {
+            File.WriteAllText(file, System.Text.Json.JsonSerializer.Serialize(b));
+        }
+        
         static void Render()
         {
             for (int row = 0; row < board.Rows; row++)
@@ -118,14 +157,110 @@ namespace cli_life
         }
         static void Main(string[] args)
         {
+            //Создаём файлы с готовыми настройками для объектов класса Board
+            File.WriteAllText("ready_settings1.json", "{\"width\":50,\"height\":20,\"cellSize\":1,\"liveDensity\":0.1}");
+            File.WriteAllText("ready_settings2.json", "{\"width\":50,\"height\":20,\"cellSize\":1,\"liveDensity\":0.2}");
+            File.WriteAllText("ready_settings3.json", "{\"width\":50,\"height\":20,\"cellSize\":1,\"liveDensity\":0.3}");
+            File.WriteAllText("ready_settings4.json", "{\"width\":50,\"height\":20,\"cellSize\":1,\"liveDensity\":0.4}");
+
+            int previousAmount = 0;
+            int boardsAmount = 4;
+            int[] numberOfGenerations = new int[boardsAmount];
+            double[] densities = new double[boardsAmount];
+            int averageTime = 0;
+
             Reset();
-            while(true)
+
+            for (int i = 0; i < boardsAmount; i++)
             {
-                Console.Clear();
-                Render();
-                board.Advance();
-                Thread.Sleep(1000);
+                int sameAmountCounter = 0;
+                int generation = 1;
+                densities[i] = 0.1 * (i + 1);
+
+                switch(i)
+                {
+                    case 0:
+                        {
+                            //Используем настройки из файла ready_settings1.json
+                            board = JsonConvert.DeserializeObject<Board>(File.ReadAllText("ready_settings1.json"))!;
+                            break;
+                        }
+                    case 1:
+                        {
+                            //Используем настройки из файла ready_settings2.json
+                            board = JsonConvert.DeserializeObject<Board>(File.ReadAllText("ready_settings2.json"))!;
+                            break;
+                        }
+                    case 2:
+                        {
+                            //Используем настройки из файла ready_settings3.json
+                            board = JsonConvert.DeserializeObject<Board>(File.ReadAllText("ready_settings3.json"))!;
+                            break;
+                        }
+                    case 3:
+                        {
+                            //Используем настройки из файла ready_settings4.json
+                            board = JsonConvert.DeserializeObject<Board>(File.ReadAllText("ready_settings4.json"))!;
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+                /*
+                 * Если кол-во элементов не меняется 10 итераций подряд,
+                 * то считаем стабильное состояние найденным
+                 */
+                while (sameAmountCounter < 10)
+                {
+                    Console.Clear();
+                    Render();
+
+                    Console.WriteLine("Поле: " + (i + 1) + " (плотность: " + (0.1 * (i + 1)) + ")"
+                    + "\nПоколение: " + generation
+                    + "\nКол-во элементов: " + ElementsAmount(board));
+
+                    if (ElementsAmount(board) == previousAmount) sameAmountCounter++;
+                    else sameAmountCounter = 0;
+
+                    Console.WriteLine("Повторений: " + sameAmountCounter + "\n");
+                    previousAmount = ElementsAmount(board);
+                    generation++;
+
+                    board.Advance();
+                    Thread.Sleep(5);
+                }
+
+                /*
+                 * При подсчёте кол-ва поколений, требуемого для достижения
+                 * стабильного состояния, вычитаем 10 повторений
+                 */
+                numberOfGenerations[i] = generation - 10;
+
+                SaveSettings("settings.json", board);
+                SaveResult("unloading.json");
             }
+
+            Console.Clear();
+            Console.WriteLine("Итоги:\n");
+            for (int a = 0; a < boardsAmount; a++)
+            {
+                Console.WriteLine("Плотность: " + (0.1 * (a + 1)) + " | поколений: " + numberOfGenerations[a]);
+                averageTime += numberOfGenerations[a];
+            }
+            Console.WriteLine("Среднее время перехода в стабильную фазу (в поколениях): " + averageTime / boardsAmount);
+
+
+            /*
+             * Построение графика зависимости кол-ва поколений, требуемого
+             * для достижения стабильного состояния, от плотности заполнения
+             */
+            ScottPlot.Plot plot = new();
+            plot.Add.Scatter(densities, numberOfGenerations, color: Colors.Red);
+            plot.Axes.Left.Label.Text = "Количество поколений";
+            plot.Axes.Bottom.Label.Text = "Плотности заполнения";
+            plot.SavePng("lifeGame.png", 1600, 900);
         }
     }
 }
