3fae34fa0bb5c85d1f05ad15eb08b6bb0c0ab800
Egor Logutov <sv9to0slav@gmail.com>
2024-05-15 14:35:14 +0300

    main

--- Life/obj/Debug/net8.0/Life.AssemblyInfo.cs
+++ Life/obj/Debug/net8.0/Life.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Life")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b61e9bfbe057be37b6976eb788390188b63339f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Life")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Life")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
