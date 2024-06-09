210b4ee2d464a840ebecca465c4ecc5ab0bf5a02
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-06-09 21:10:29 +0300

    Update UnitTest.cs

--- Tests/UnitTest.cs
+++ Tests/UnitTest.cs
@@ -11,135 +11,147 @@ namespace Client.Tests
     [TestClass()]
     public class Test
     {
-        [TestMethod()]
-        public void PrefabTest()
+    [TestMethod()]
+    public void Test1()
+    {
+        Cell cell = new Cell(false);
+        Assert.AreEqual(cell.Is_alive, false);
+    }
+    [TestMethod()]
+    public void Test2()
+    {
+        Templates templates = new Templates();
+        string[] myStrings = { "10", "01" };
+        int[,] mtr = { { 1, 1 }, { 1, 1 } };
+        var go = templates.Get_matrix(myStrings);
+        string fgf = "";
+        string gfg = "";
+        foreach (var fg in go)
         {
-            Cell cell = new Cell(false);
-            Assert.AreEqual(cell.IsAlive,false);
+            fgf += fg.ToString();
         }
-        [TestMethod()]
-        public void Test2()
+        foreach (var ds in mtr)
         {
-            Templates templates = new Templates();
-            string[] myStrings = { "11", "11"};
-            int[,] mtr = { { 1, 1 }, { 1, 1 } };
-            var go = templates.GetMatrix(myStrings);
-            string fgf = "";
-            string gfg = "";
-            foreach(var fg in go)
-            {
-                fgf += fg.ToString();
-            }
-            foreach(var ds in mtr)
-            {
-                gfg += ds.ToString();
-            }
-            Assert.AreEqual(fgf, gfg);
+            gfg += ds.ToString();
         }
-        [TestMethod()]
-        public void Test3()
+        Assert.AreEqual(fgf, gfg);
+    }
+    [TestMethod()]
+    public void Test3()
+    {
+        Templates templates = new Templates();
+        string[] myStrings = { "100", "110", "111" };
+        int[,] mtr = { { 1, 0, 0 }, { 1, 1, 0 }, { 1, 1, 1 } };
+        var go = templates.Get_matrix(myStrings);
+        string fgf = "";
+        string gfg = "";
+        foreach (var fg in go)
         {
-            Templates templates = new Templates();
-            string[] myStrings = { "110", "110","111" };
-            int[,] mtr = { { 1, 1,0 }, { 1, 1,0 }, {1,1,1} };
-            var go = templates.GetMatrix(myStrings);
-            string fgf = "";
-            string gfg = "";
-            foreach (var fg in go)
-            {
-                fgf += fg.ToString();
-            }
-            foreach (var ds in mtr)
-            {
-                gfg += ds.ToString();
-            }
-            Assert.AreEqual(fgf, gfg);
+            fgf += fg.ToString();
         }
-        [TestMethod()]
-        public void Test4()
+        foreach (var ds in mtr)
         {
-            Templates templates = new Templates(out string str);
-            Assert.AreEqual(str, "Выполнено");
+            gfg += ds.ToString();
         }
-        [TestMethod()]
-        public void Test5()
+        Assert.AreEqual(fgf, gfg);
+    }
+    [TestMethod()]
+    public void Test4()
+    {
+        Templates templates = new Templates(out string str);
+        Assert.AreEqual(str, "done");
+    }
+    [TestMethod()]
+    public void Test5()
+    {
+        Templates templates = new Templates();
+        string[] myStrings = { "111", "111", "111" };
+        int[,] mtr = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+        var go = templates.Get_matrix(myStrings);
+        string fgf = "";
+        string gfg = "";
+        foreach (var fg in go)
         {
-            int[,] mtr = { { 1, 1 }, { 1, 1 } };
-            Prefab prefab = new(2, 2, mtr, "Я хочу домой");
-            Assert.AreEqual(prefab._height,2);
+            fgf += fg.ToString();
         }
-        [TestMethod()]
-        public void Test6()
+        foreach (var ds in mtr)
         {
-            int[,] mtr = { { 1, 1 }, { 1, 1 } };
-            Prefab prefab = new(2, 2, mtr, "Я хочу домой");
-            Assert.AreEqual(prefab._name, "Я хочу домой");
+            gfg += ds.ToString();
         }
-        [TestMethod()]
-        public void Test7()
-        {
-            int[,] mtr = { { 1, 1 }, { 1, 1 } };
-            Prefab prefab = new(2, 2, mtr, "Я хочу домой");
-            Assert.AreEqual(prefab.CheckPrefab(new Board(1,1,1)),0);
-        }
-        [TestMethod()]
-        public void Test8()
-        {
-            int[,] mtr = { { 1, 1, 1 }, { 1, 1, 1 } };
-            Prefab prefab = new(2, 2, mtr, "Я хочу домой");
-            Assert.AreEqual(prefab.CheckPrefab(new Board(1, 1, 1)), 0);
-        }
-        [TestMethod()]
-        public void Test9()
-        {
-            var board = new Board(1, 1, 1);
-            board.Advance();
-            Assert.AreEqual(board.CheckActive(),true );
-        }
-        [TestMethod()]
-        public void Test101()
-        {
-            var board = new Board(1, 1, 1);
-            var str = board.BoardSave();
-            Assert.AreEqual(str,"Сохранено");
-        }
-        [TestMethod()]
-        public void Test11()
-        {
-            var board = new Board(1, 1, 1);
-            var str = board.Discharge();
-            Assert.AreEqual(str, "Выгружено");
-        }
-        [TestMethod()]
-        public void Test12()
-        {
-            var board = new Board(1, 1, 1);
-            var str = Board.Loading();
-            Assert.IsTrue(str is Board);
-        }
-        [TestMethod()]
-        public void Test13()
-        {
-            var board = new Board(1, 1, 1);
-            var str = Board.Loading();
-            Assert.IsTrue(str.Width==30);
-        }
-        [TestMethod()]
-        public void Test14()
-        {
-            var board = new Board(1, 1, 1);
-            var str = Board.Loading();
-            Cell cell = new Cell(false);
-            Assert.AreEqual(cell.IsAlive, false);
-        }
-        [TestMethod()]
-        public void Test15()
-        {
-            var str = Board.Loading();
-            Cell cell = new Cell(false);
-            var board = new Board(1, 1, 1);
-            board.Advance();
-            Assert.AreEqual(board.CheckActive(), true);
-        }
+        Assert.AreEqual(fgf, gfg);
+    }
+    [TestMethod()]
+    public void Test6()
+    {
+        Templates templates = new Templates(out string str);
+        Assert.AreNotEqual(str, "save");
+    }
+    [TestMethod()]
+    public void Test7()
+    {
+        var board = new Board(1, 1, 1);
+        board.Advance();
+        Assert.AreNotEqual(board.Check_active(), false);
+    }
+    [TestMethod()]
+    public void Test8()
+    {
+        var board = new Board(1, 1, 1);
+        board.Advance();
+        Assert.AreEqual(board.Check_active(), true);
+    }
+    [TestMethod()]
+    public void Test9()
+    {
+        var board = new Board(1, 1, 1);
+        var str = board.Board_save();
+        Assert.AreEqual(str, "save");
+    }
+    [TestMethod()]
+    public void Test10()
+    {
+        var board = new Board(1, 1, 1);
+        var str = board.Discharge();
+        Assert.AreEqual(str, "done");
+    }
+    [TestMethod()]
+    public void Test11()
+    {
+        var board = new Board(1, 1, 1);
+        var str = Board.Loading();
+        Assert.IsTrue(str is Board);
+    }
+    [TestMethod()]
+    public void Test12()
+    {
+        var board = new Board(1, 1, 1);
+        var str = Board.Loading();
+        Assert.IsTrue(str.Width == 30);
+    }
+    [TestMethod()]
+    public void Test13()
+    {
+        var board = new Board(1, 1, 1);
+        var str = Board.Loading();
+        Cell cell = new Cell(false);
+        Assert.AreEqual(cell.Is_alive, false);
+    }
+    [TestMethod()]
+    public void Test14()
+    {
+        var board = new Board(1, 1, 1);
+        var str = Board.Loading();
+        Cell cell = new Cell(false);
+        Assert.AreNotEqual(cell.Is_alive, true);
+    }
+    [TestMethod()]
+    public void Test15()
+    {
+        var str = Board.Loading();
+        Cell cell = new Cell(false);
+        var board = new Board(1, 1, 1);
+        board.Advance();
+        Assert.AreEqual(board.Check_active(), true);
     }
 }
+
